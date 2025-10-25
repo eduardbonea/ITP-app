b7401fe8f38e54c6a801f397c95755510d56166d
Eduard Bonea <eduardbonea@gmail.com>
2025-10-25 17:14:09 +0300

    typo

--- backend/twilio-service.js
+++ backend/twilio-service.js
@@ -17,7 +17,7 @@ const client = twilio(accountSid, authToken);
 const sequelize = new Sequelize(
   process.env.DB_NAME, 
   process.env.DB_USER, 
-  process.env.DB_PASS, 
+  process.env.DB_PASSWORD, 
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
